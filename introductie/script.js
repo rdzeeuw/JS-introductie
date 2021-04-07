c7b24fc0e8db704d972cc14a24995f38a3c316d0
rdzeeuw <77979140+rdzeeuw@users.noreply.github.com>
2021-04-07 17:06:16 +0200

    Add files via upload

---
+++ introductie/script.js
@@ -0,0 +1,18 @@
+console.log("Hello Winc Academy");
+let name = "Robin";
+console.log(name);
+
+//7 en 8 
+console.log(4+4);
+console.log("4"+"4");
+console.log(4*2);
+console.log(10%100)
+console.log(16/2);
+
+//9
+const age = 33;
+console.log(typeof age);
+
+//10
+const ageText = "drieendertig";
+console.log(typeof ageText);
